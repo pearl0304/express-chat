b5dc954d0b9a348d56526734d122005d96555df6
Kylie <kylie@comets.kr>
2021-11-04 17:59:11 +0900

    modify

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,11 +1,14 @@
-const user_id = $("input[name=user_id]").val().length
-const user_nick = $("input[name=user_nick]").val().length
-const user_pw = $("input[name=user_pw]").val().length
+
 
 $(function(){checkSignForm()})
 
 function checkSignForm(){
     $("#signUpform").on("submit",function(){
+
+        const user_id = $("input[name=user_id]").val().length
+        const user_nick = $("input[name=user_nick]").val().length
+        const user_pw = $("input[name=user_pw]").val().length
+        
         if(user_id == 0){
             alert('Please enter ID you want to use')
             return false
